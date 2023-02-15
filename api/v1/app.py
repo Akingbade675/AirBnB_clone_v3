e6b1a3604992182b3c1325163c03d7290138284a
Akingbade675 <akingbadebasit26@gmail.com>
2023-02-15 21:22:42 +0000

    updated city view

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,15 @@
 '''main app module'''
 
 from os import getenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 host = getenv("HBNB_API_HOST", default="0.0.0.0")
 port = int(getenv("HBNB_API_PORT", default=5000))
@@ -21,7 +24,7 @@ def close(exc):
 @app.errorhandler(404)
 def page_not_found(e):
     '''404 error handler'''
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     app.run(host=host, port=port, threaded=True)
